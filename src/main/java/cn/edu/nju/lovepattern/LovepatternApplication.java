b0b20b6aa08c8cc2422a0fb9d3ac76358fdd07c1
李晓冬 <lixiao1k@163.com>
2019-03-18 10:59:52 +0800

    add gitignore

---
+++ src/main/java/cn/edu/nju/lovepattern/LovepatternApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.lovepattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovepatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovepatternApplication.class, args);
+	}
+
+}
